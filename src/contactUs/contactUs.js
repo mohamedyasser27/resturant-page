9b636aacb3989b7e3ffc6a384903e56ac6d17f16
mohamedyasser27 <my272000@gmail.com>
2022-06-07 19:44:57 +0200

    add dist folder

--- src/contactUs/contactUs.js
+++
@@ -1,9 +0,0 @@
-import { toggleClass } from "../resuable.js";
-import "./contactUs.css";
-
-export default function contactUsModule() {
-  let contactUs = document.createElement("div");
-  toggleClass("ContactUs", contactUs);
-
-  return contactUs;
-}
